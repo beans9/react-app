4918b0d0b19acb5f5a683cde87f0aa00a564e9d0
beans9 <ahmax95@gmail.com>
2022-04-04 04:41:13 +0900

    Updates

---
+++ precache-manifest.a603b89783476d96fef9cb990fda21af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "97cdaa9a68830206b047",
+    "url": "/static/js/main.97cdaa9a.chunk.js"
+  },
+  {
+    "revision": "f0e60864e8a43abb55b2",
+    "url": "/static/js/1.f0e60864.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1496e2e6de9ba8a4f4418e89a592439",
+    "url": "/index.html"
+  }
+];
